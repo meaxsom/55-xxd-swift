7b31f99808bb8929f247ef98138808c618612fbc
Matthew E. Axsom <git@tipsydeveloper.io>
2024-04-02 15:51:31 -0400

    test new extensions

--- xxdTest/xxdTest.swift
+++ xxdTest/xxdTest.swift
@@ -7,21 +7,60 @@
 
 import XCTest
 
-final class xxdTest: XCTestCase {
+extension StringProtocol {
+    var hexaBytes: [UInt8] {
+        var startIndex = self.startIndex
+        return stride(from: 0, to: count, by: 2).compactMap { _ in
+            let endIndex = index(startIndex, offsetBy: 2, limitedBy: self.endIndex) ?? self.endIndex
+            defer { startIndex = endIndex }
+            return UInt8(self[startIndex..<endIndex], radix: 16)
+        }
+    }
+}
 
+final class xxdTest: XCTestCase {
+    
+    private static let kData = "7801edd0b10900201003407112cb5fc3"
+    private static let kAsciiData = "x...... ..@q.._."
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-
+    
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() throws {
-        var greeting = Greeting()
-        XCTAssertNotNil(greeting.name)
+    
+    func testChunked() throws {
+        let theBytes = xxdTest.kData.hexaBytes
         
-        greeting.name = "Matthew"
-        XCTAssertTrue(greeting.name == "Matthew")
+        var theGroups = theBytes.chunked(into: 2)
+        XCTAssertEqual(theGroups.count, 8)
+        
+        theGroups = theBytes.chunked(into: 4)
+        XCTAssertEqual(theGroups.count, 4)
+        
+        theGroups = theBytes.chunked(into: 1)
+        XCTAssertEqual(theGroups.count, 16)
+        
+        theGroups = theBytes.chunked(into: 3)
+        XCTAssertEqual(theGroups.count, 6)
+    }
+    
+    func testToHexString() throws {
+        let theBytes = xxdTest.kData.hexaBytes
+        let theGroups = theBytes.chunked(into: 2)
+
+        XCTAssertEqual(theGroups.count, 8)
+        
+        XCTAssertEqual(theGroups[0].toHexString(), "7801")
+        XCTAssertEqual(theGroups[1].toHexString(), "edd0")
+        XCTAssertEqual(theGroups[2].toHexString(), "b109")
+        XCTAssertEqual(theGroups[7].toHexString(), "5fc3")
+    }
+    
+    func testStringFromBytes() throws {
+        let theBytes = xxdTest.kData.hexaBytes
+        XCTAssertEqual(String(theBytes), xxdTest.kAsciiData)
     }
 }
