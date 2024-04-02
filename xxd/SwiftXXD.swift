570465bbe79f80e91b189270c4c25371b14be6d2
Matthew E. Axsom <git@tipsydeveloper.io>
2024-04-02 15:53:15 -0400

    should complete Step 1

--- xxd/SwiftXXD.swift
+++ xxd/SwiftXXD.swift
@@ -11,14 +11,37 @@ import ArgumentParser
 
 @main
 struct SwiftXXD : ParsableCommand {
-    @Argument var name: String
-    @Flag var log: Bool = false
-    
+    @Argument var filename: String
+
+    static let kDefaultDataSize = 16
+    static let kDefaultGroupSize = 4
+
+    static let kDefaultFileOffset : UInt64 = 0
+    static let kDefaultDataIncrementSize : UInt64 = UInt64(kDefaultDataSize)
+
     func run() throws {
-        
-        print("Hello World to \(name)!")
-        if (log) {
-            MyLogger.log.debug("Hello World")
+        do {
+            let fileHandle = try FileHandle(forReadingFrom: URL(fileURLWithPath: filename))
+            
+            var fileOffset = SwiftXXD.kDefaultFileOffset
+            if (fileOffset > 0) {
+                try fileHandle.seek(toOffset: fileOffset)
+            }
+            
+            while let values = fileHandle.readUInt8s(ofSize: SwiftXXD.kDefaultDataSize) {
+                let theView = String(values)
+                let theOffset = String(format: "%08X", fileOffset)
+                
+                let theLine = values.chunked(into: SwiftXXD.kDefaultGroupSize).map { $0.toHexString() }.joined(separator: " ")
+
+                print("\(theOffset): \(theLine)  \(theView)")
+                fileOffset += SwiftXXD.kDefaultDataIncrementSize
+            }
+            
+            fileHandle.closeFile()
+            
+        } catch {
+            print("Error reading file: \(error)")
         }
     }
 }
